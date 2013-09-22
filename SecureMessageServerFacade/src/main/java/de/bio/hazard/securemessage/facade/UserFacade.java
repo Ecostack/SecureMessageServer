188a9550a805b91ce9e1f1b375676db84682468a
Nico <Nico@NigGo>
2013-09-22 03:55:08 +0200

    BCrypt PW beim hinzufügen eines Benutzers Statics BCRYPT_ROUNDS

--- SecureMessageServerFacade/src/main/java/de/bio/hazard/securemessage/facade/UserFacade.java
+++ SecureMessageServerFacade/src/main/java/de/bio/hazard/securemessage/facade/UserFacade.java
@@ -1,5 +1,11 @@
 package de.bio.hazard.securemessage.facade;
 
+import java.io.IOException;
+import java.security.InvalidKeyException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -27,18 +33,22 @@ public class UserFacade {
 
     public UserWebserviceReturnDTO getUserByUsername(UserWebserviceDTO pUserWebserviceDTO) throws UserNotFoundException {
 	try {
-	    Config lcServerPrivateKey = configService.getConfigByEnumType(ConfigType.SERVER_PRIVATE_KEY);
-	    byte[] lcSymmetricKey = encryptionObjectModifier.asymmetricDecryptToByte(pUserWebserviceDTO.getSymEncryptionKey(), lcServerPrivateKey.getValue(), true);
-	    UserWebserviceReturnDTO lcUserWebserviceReturnDTO=new UserWebserviceReturnDTO();
-	    User lcUser=userService.getUserByUsername(encryptionObjectModifier.symmetricDecrypt(pUserWebserviceDTO.getUsername(), lcSymmetricKey));
-	    //TODO über Token das Device suchen und mit dessen PK verschlüsseln (symmetrisch)
-	    lcUserWebserviceReturnDTO.setPublicKey(encryptionObjectModifier.encodeBase64(lcUser.getPublicAsymmetricKey()));
-	    return lcUserWebserviceReturnDTO;
+	    return transformToUserWebserviceReturnDTO(pUserWebserviceDTO);
 	}
 	catch (Exception e) {
 	    // TODO handle exception
 	    System.err.println(e.toString());
 	}
 	throw new UserNotFoundException();
+    }
+
+    private UserWebserviceReturnDTO transformToUserWebserviceReturnDTO(UserWebserviceDTO pUserWebserviceDTO) throws IOException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+	Config lcServerPrivateKey = configService.getConfigByEnumType(ConfigType.SERVER_PRIVATE_KEY);
+	byte[] lcSymmetricKey = encryptionObjectModifier.asymmetricDecryptToByte(pUserWebserviceDTO.getSymEncryptionKey(), lcServerPrivateKey.getValue(), true);
+	UserWebserviceReturnDTO lcUserWebserviceReturnDTO=new UserWebserviceReturnDTO();
+	User lcUser=userService.getUserByUsername(encryptionObjectModifier.symmetricDecrypt(pUserWebserviceDTO.getUsername(), lcSymmetricKey));
+	//TODO über Token das Device suchen und mit dessen PK verschlüsseln (symmetrisch)
+	lcUserWebserviceReturnDTO.setPublicKey(encryptionObjectModifier.encodeBase64(lcUser.getPublicAsymmetricKey()));
+	return lcUserWebserviceReturnDTO;
     }
 }
--- SecureMessageServerService/src/main/java/de/bio/hazard/securemessage/util/Statics.java
+++ SecureMessageServerService/src/main/java/de/bio/hazard/securemessage/util/Statics.java
@@ -1,6 +1,5 @@
 package de.bio.hazard.securemessage.util;
 
 public interface Statics {
-	int CONFIG_SERVER_KEY_PRIVATE = 10;
-	int CONFIG_SERVER_KEY_PUBLIC = 20;
+	int CONFIG_BCRYPT_ROUNDS=10;
 }
