8aa1d3b5bf916af9872c8b1d456e9a9a0947a57f
N4wuko <N4wuko@192.168.0.105>
2013-10-02 22:27:11 +0200

    NewDeviceWebserviceReturnDTO für Client sichtbar

--- SecureMessageServerWebservice/src/main/java/de/bio/hazard/securemessage/webservice/AuthenticationWebservice.java
+++ SecureMessageServerWebservice/src/main/java/de/bio/hazard/securemessage/webservice/AuthenticationWebservice.java
@@ -11,10 +11,13 @@ import de.bio.hazard.securemessage.dto.authentication.AuthenticationStepOneRetur
 import de.bio.hazard.securemessage.dto.authentication.AuthenticationStepTwoDTO;
 import de.bio.hazard.securemessage.dto.authentication.AuthenticationStepTwoReturnDTO;
 import de.bio.hazard.securemessage.dto.device.NewDeviceWebserviceDTO;
+import de.bio.hazard.securemessage.dto.device.NewDeviceWebserviceReturnDTO;
 import de.bio.hazard.securemessage.dto.user.NewUserWebserviceDTO;
 import de.bio.hazard.securemessage.facade.AuthenticationFacade;
 import de.bio.hazard.securemessage.facade.NewDeviceFacade;
 import de.bio.hazard.securemessage.facade.NewUserFacade;
+import de.bio.hazard.securemessage.facade.exception.DeviceNotFoundException;
+import de.bio.hazard.securemessage.facade.exception.EncryptionException;
 
 @WebService
 @Component
@@ -35,19 +38,19 @@ public class AuthenticationWebservice {
 	}
 
 	@WebMethod
-	public void addNewDevice(NewDeviceWebserviceDTO pNewDevice) {
-		newDeviceFacade.addNewDevice(pNewDevice);
+	public NewDeviceWebserviceReturnDTO addNewDevice(NewDeviceWebserviceDTO pNewDevice) {
+		return newDeviceFacade.addNewDevice(pNewDevice);
 
 	}
 
 	public AuthenticationStepOneReturnDTO authenticateStepOne(
-			AuthenticationStepOneDTO pAuthenticationStepOneDTO) {
+			AuthenticationStepOneDTO pAuthenticationStepOneDTO) throws EncryptionException, DeviceNotFoundException {
 		return authenticationFacade
 				.authenticateStepOne(pAuthenticationStepOneDTO);
 	}
 
 	public AuthenticationStepTwoReturnDTO authenticateStepTwo(
-			AuthenticationStepTwoDTO pAuthenticationStepTwoDTO) {
+			AuthenticationStepTwoDTO pAuthenticationStepTwoDTO) throws EncryptionException {
 		return authenticationFacade
 				.authenticateStepTwo(pAuthenticationStepTwoDTO);
 	}
